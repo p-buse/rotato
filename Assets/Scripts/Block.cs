da8096b1c8acdafa8b0ba1e32326a8e5927bacac
Paul Buser <pbuser@oberlin.edu>
2014-10-02 19:32:10 -0400

    Added static block scripts and prefab.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public abstract class Block : MonoBehaviour
+{
+    protected int x;
+    protected int y;
+    /// <summary>
+    /// Rotates the block, given a center position and a direction.
+    /// Direction can be "cw" or "ccw"
+    /// </summary>
+    /// <param name="xCenter">The x position of the point to rotate around</param>
+    /// <param name="yCenter">The y position of the point to rotate around</param>
+    /// <param name="direction">The direction (can be "cw" or "ccw")</param>
+    public abstract void RotateTo(int xCenter, int yCenter, string direction);
+    public abstract bool invalidatesRotation();
+
+}
+    
---
+++ Assets/Scripts/StaticBlock.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticBlock : Block {
+
+    public override void RotateTo(int x, int y, string direction)
+    {
+        // Rotation code here
+    }
+
+    public override bool invalidatesRotation()
+    {
+        return false;
+    }
+}
