59287a50bd22020f74b4c6644a5b3021d2d5f4cc
Paul Buser <pbuser@oberlin.edu>
2014-10-04 18:33:24 -0400

    Added (wonky) platforming.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour {
 
     public Int2 GetRoundedPosition()
     {
-        //TODO actually do this.
-        return new Int2(0, 0);
+        return new Int2(transform.position.x, transform.position.y);
     }
+
 }
