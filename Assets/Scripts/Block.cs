74fa50df24b4cbcd71a5fdffbb856e4893aa0d5e
Daniel Miller-Medzon <dmillerm@oberlin.edu>
2014-10-05 13:54:20 -0400

    fixed weirdness after merge

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,14 +8,11 @@ public class Block : AbstractBlock {
 
     }
 
-<<<<<<< HEAD
     /*public override void AnimateFrameOfRotation(Int2 center, string direction, float time)
     {
         throw new System.NotImplementedException();
     }*/
-
-=======
->>>>>>> 0d9ff62ef7f39ffefb3f152f6282260488330536
+		
     public override bool invalidatesRotation()
     {
         return false;
