bbde3b5892d1fd8b87e45b76532b4a8e85dbdc3d
Damien Kirk <dkirk@oberlin.edu>
2014-10-05 01:16:26 -0400

    Made the grid in BlockManager public so I could access and change it from FallingBlock.  Changed the value type of grid to AbstractBlock.

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System;
 
 public class BlockManager : MonoBehaviour {
-    private Dictionary<Int2, Block> grid;
+    public Dictionary<Int2, AbstractBlock> grid; // changed to public so I can see and change it in falling blocks.  Also changed the value type to AbstractBlock.
     Player player;
 
     private bool isValidRotation(Vector2 center, int direction)
@@ -19,9 +19,9 @@ public class BlockManager : MonoBehaviour {
         player = FindObjectOfType<Player>();
         // Populate our grid with the blocks in the scene
         // FindObjectsOfType is an expensive operation, so we only run it once per scene
-        grid = new Dictionary<Int2, Block>();
-        Block[] blocks = GameObject.FindObjectsOfType<Block>();
-        foreach (Block b in blocks)
+        grid = new Dictionary<Int2, AbstractBlock>();
+        Block[] blocks = GameObject.FindObjectsOfType<AbstractBlock>();
+        foreach (AbstractBlock b in blocks)
         {
             Int2 blockPosition = new Int2(b.transform.position.x, b.transform.position.y);
             // Detect overlapping blocks
