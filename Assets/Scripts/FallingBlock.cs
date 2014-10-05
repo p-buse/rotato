d8a78e38c6f541fb0d16e07fea8bfa932447ca29
Damien Kirk <dkirk@oberlin.edu>
2014-10-05 17:45:07 -0400

    Allowed block to begin falling and made them stop if they land on a player.

--- Assets/Scripts/FallingBlock.cs
+++ Assets/Scripts/FallingBlock.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 
 public class FallingBlock : AbstractBlock {
 
-    float fallClock;
-    BlockManager blockManager;
-	bool whichHalf = true;
-	Int2 location;
+    public float fallClock;
+    public BlockManager blockManager;
+	public bool whichHalf = true;
+	public Int2 location;
 
 	void Start() {
         fallClock = 1f;
@@ -25,27 +25,25 @@ public class FallingBlock : AbstractBlock {
 
 	void Update() {
 		if (!gameManager.rotationHappening) {
+			Dictionary<Int2, AbstractBlock> grid = blockManager.grid;
 			if (fallClock >= 0.0f) {
 				fallClock += Time.deltaTime*3;
 				if (fallClock >= 1.0f && whichHalf) {
 					fallClock -= 1.0f;
 					whichHalf = false;
-					Dictionary<Int2, AbstractBlock> grid = blockManager.grid;
 					if (grid.ContainsKey(location) && grid[location] == this) {
 						grid.Remove(location);
 					}
 					location.y--;
-					if (grid.ContainsKey(location) && grid[location] != this) {
+					if (!grid.ContainsKey(location) || grid[location] != this) {
 						grid.Remove(location);
 						grid.Add(location, this);
 					}
-					Int2 below = new Int2(location.x, location.y-1);if (!grid.ContainsKey(below)) {
-							grid.Add(below, this);
-						}
-					if (!grid.ContainsKey(below)) {
+					Int2 below = new Int2(location.x, location.y-1);
+					if (!grid.ContainsKey(below) && !below.Equals(blockManager.player.GetRoundedPosition())) {
 						grid.Add(below, this);
 					}
-					else if ((grid[below] as FallingBlock == null || (grid[below] as FallingBlock).fallClock < 0.0f)) {
+					else if (below.Equals(blockManager.player.GetRoundedPosition()) || (grid.ContainsKey(below) && ((grid[below] as FallingBlock == null || (grid[below] as FallingBlock).fallClock < 0.0f)))) {
 						fallClock = -1.0f;
 						transform.position = new Vector3(location.x, location.y, 0.0f);
 					}
@@ -58,7 +56,7 @@ public class FallingBlock : AbstractBlock {
 				}
 			}
 			Int2 belowThis = new Int2(location.x, location.y-1);
-			if ((!grid.ContainsKey(belowThis) || (grid[belowThis] as FallingBlock != null && (grid[belowThis] as FallingBlock).fallClock >= 0.0f)) && !belowThis.Equals(blockManager.player.GetRoundedPosition())) {
+			if (fallClock <= 0.0f && ((!grid.ContainsKey(belowThis) || (grid[belowThis] as FallingBlock != null && (grid[belowThis] as FallingBlock).fallClock >= 0.0f)) && !belowThis.Equals(blockManager.player.GetRoundedPosition()))) {
 				fallClock = 0.0f;
 			}
 		}
