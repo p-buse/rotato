08f3edefd16e97e8f457bbe8118941ca8cc435c4
Paul Buser <pbuser@oberlin.edu>
2014-10-05 01:00:57 -0400

    Cleaned up rotation code in AbstractBlock.

--- Assets/Scripts/AbstractBlock.cs
+++ Assets/Scripts/AbstractBlock.cs
@@ -6,7 +6,7 @@ public abstract class AbstractBlock : MonoBehaviour
     protected GameManager gameManager;
 
 	//blockModel needs to be created, not sure how to start that in prefab world.
-	public blockModel model;
+	public Transform blockSprite;
 
 	public float orientation; //starts at 0, +1 = 1 90-degree turn ccw ?  we can tweak what this means. 
 	//in analog of position, probably want this to be discrete, while model has continuous EulerAngles instead
@@ -14,6 +14,7 @@ public abstract class AbstractBlock : MonoBehaviour
     void Start()
     {
         this.gameManager = GameObject.FindObjectOfType<GameManager>();
+        this.blockSprite = transform.Find("blockSprite");
     }
 
 	/// <summary>
@@ -23,17 +24,17 @@ public abstract class AbstractBlock : MonoBehaviour
 	/// <param name="center">Center.</param>
 	/// <param name="direction">Direction.</param>
 	/// <param name="time">Time.</param>
-	public abstract void AnimateFrameOfRotation (Int2 center, string direction, float time)
+	public virtual void AnimateFrameOfRotation (Int2 center, int direction, float time)
 	{
-		int dx = transform.position.x-center.x;
-		int dy = transform.position.y-center.y;
-		int newdx = –1.0*direction*dy;
-		int newdy = direction*dx;
-		Vector3 startVec = Vector3(dx,dy,0);
-		Vector3 endVec = Vector3(newdx,newdy,0);
+		int dx = Mathf.RoundToInt(transform.position.x) - center.x;
+		int dy = Mathf.RoundToInt(transform.position.y) - center.y;
+		int newdx = -1 * direction * dy;
+		int newdy = direction * dx;
+		Vector3 startVec = new Vector3(dx,dy,0);
+		Vector3 endVec = new Vector3(newdx,newdy,0);
 		
 		//I'm pretty sure this is right...
-		model.transform.position = 0.5*(Mathf.Cos(t*Mathf.PI/2.0)*startVec + Mathf.Sin(t*Mathf.PI/2.0)*endVec) + Vector3(-dx,-dy,0);
+		blockSprite.transform.position = 0.5f*(Mathf.Cos(time * Mathf.PI / 2.0f)*startVec + Mathf.Sin(time*Mathf.PI/2.0f)*endVec) + new Vector3(-dx,-dy,0);
 		
 		//the above is absolute, this is relative... not sure how I could do this (facing) absolutely
 		//model.transform.rotation += (direction*Time.deltaTime*90.0);
@@ -55,11 +56,11 @@ public abstract class AbstractBlock : MonoBehaviour
 	/// <returns> The destination Int2 of this block after this rotation</returns>
 	/// <param name="center">The center position.</param>
 	/// <param name="dir">Dir.</param>
-	public static Int2 posAfterRotation(Int2 center, int dir)
+	public virtual Int2 posAfterRotation(Int2 center, int dir)
 	{
-		int dx = transform.position.x-center.x;
-		int dy = transform.position.y-center.y;
-		int newx = center.x – (dir*dy);
+		int dx = Mathf.RoundToInt(transform.position.x) -center.x;
+		int dy = Mathf.RoundToInt(transform.position.y) - center.y;
+		int newx = center.x - (dir*dy);
 		int newy = center.y + (dir*dx);
 		return new Int2(newx, newy);
 	}
@@ -69,11 +70,11 @@ public abstract class AbstractBlock : MonoBehaviour
 	/// </summary>
 	/// <param name="center">Center.</param>
 	/// <param name="dir">Dir.</param>
-	public static void finishRotation(Int2 center, int dir)
+	public virtual void finishRotation(Int2 center, int dir)
 	{
 		Int2 end = posAfterRotation (center, dir);
-		Transform.position = Vector3 (end.x, end.y, 0);
-		model.transform.position = Vector3(0,0,0);
+		transform.position = new Vector3 (end.x, end.y, 0);
+		blockSprite.transform.position = new Vector3(0,0,0);
 		orientation += dir;
 	}
 
