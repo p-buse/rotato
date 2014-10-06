c3699ce5953268adab5a6104d1998ecd53fd999e
David Miller <davidmiller64@gmail.com>
2014-10-05 21:42:32 -0400

    Added click detection to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,17 +32,46 @@ public class GameManager : MonoBehaviour
         {
             case RotationMode.playing:
                 {
+					if(Input.GetMouseButtonDown(0))
+					{
+						Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+						int x = Mathf.RoundToInt(worldPos.x);
+						int y = Mathf.RoundToInt(worldPos.y);
+						if(isValidCenter(new Int2(x,y))){
+							gameState = RotationMode.frozen;
+						}
+						
+					}
                     break;
                 }
-            case RotationMode.frozen:
+
+            case RotationMode.frozen: //game is frozen, left-click is held, but no rotation is happening
                 {
+					if(Input.GetMouseButtonUp(0))
+					{
+						gameState = RotationMode.playing;
+					}
+					//detect keyboard input, call blockmanager rotation, set gameState to RotationMode.rotating
                     break;
                 }
+
             case RotationMode.rotating:
                 {
+					//check BlockManager for rotation clock, if it's done set gameState back to RotationMode.frozen
                     break;
                 }
         }
     }
+
+	public static bool isValidCenter(Int2 xy){
+		Int2 playerPos = blockManager.player.GetRoundedPosition();
+		int absDx = Mathf.Abs (xy.x - playerPos.x);
+		int absDy = Mathf.Abs (xy.y - playerPos.y);
+		if ((absDx <= 2 && absDy <= 2) && (absDx == 2 || absDy == 2 || (absDx == 0 && absDy == 0))) 
+		{
+			return true;
+		}
+		return false;
+	}
 
 }
