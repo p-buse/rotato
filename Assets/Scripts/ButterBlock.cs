06ddfee2a3edb75e55c9d9e4a1466032b5904f35
Damien Kirk <dkirk@oberlin.edu>
2014-10-06 16:31:10 -0400

    Added ButterBlock script.  Currently identical to FixedBlock, needs functionality and texture.

---
+++ Assets/Scripts/ButterBlock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButterBlock : AbstractBlock {
+	
+	public override bool invalidatesRotation() {
+		return false;
+	}
+	
+	public override bool isRotable() {
+		return false;
+	}
+	
+	public override void AnimateFrameOfRotation (Int2 center, int direction, float time) {}
+	
+	public override Int2 posAfterRotation(Int2 center, int dir) {
+		return new Int2 (transform.position.x, transform.position.y);
+	}
+}
