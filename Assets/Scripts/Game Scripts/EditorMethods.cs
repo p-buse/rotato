d12ecfeb7a230eef4e87edcde92a0adba5bea638
David Miller <davidmiller64@gmail.com>
2014-11-03 22:01:21 -0500

    fixed more casting issues

--- Assets/Scripts/Game Scripts/EditorMethods.cs
+++ Assets/Scripts/Game Scripts/EditorMethods.cs
@@ -7,7 +7,7 @@ public class EditorMethods : MonoBehaviour
 	GameManager gameManager;
 	BlockManager blockManager;
 	public enum BlockType{regular, cracked, falling, cannon, fix, mirror, laser}
-
+	
 
 	/// <summary>
 	/// Creates a new  prefab of the type passed, at the given position.
@@ -21,11 +21,10 @@ public class EditorMethods : MonoBehaviour
 
 		newObject.transform.position = new Vector3 (pos.x, pos.y, 0);
 
-		if(newObject is AbstractBlock)
-		{
-			AbstractBlock thisBlock = newObject as AbstractBlock;
-	
-			BlockManager.grid.Add(pos, thisBlock);
+		AbstractBlock blockScript = newObject.GetComponent<AbstractBlock> ();
+		if(blockScript!=null)
+		{	
+			blockManager.grid.Add(pos, blockScript);
 		}
 					
 	}
@@ -36,14 +35,16 @@ public class EditorMethods : MonoBehaviour
 	/// <param name="prefab">Prefab.</param>
 	private void removeObject(GameObject ob)
 	{
-		if(ob is AbstractBlock)
+		AbstractBlock blockScript = ob.GetComponent<AbstractBlock> ();
+		CrawlerSegment crawler = ob.GetComponent<CrawlerSegment> ();
+		if(blockScript!=null)
 		{
-			blockManager.grid.Remove((ob as AbstractBlock).GetCurrentPosition);
+			blockManager.grid.Remove(blockScript.GetCurrentPosition());
 			Destroy(ob);
 		}
-		else if(ob is CrawlerSegment)
+		else if(crawler!=null)
 		{
-			(ob as CrawlerSegment).dieSafely();
+			crawler.dieSafely();
 		}
 		else{
 			Destroy (ob);
