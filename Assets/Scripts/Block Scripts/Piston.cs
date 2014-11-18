191f0b13d85854b19104f8969d4f2754b399badf
Daniel Miller-Medzon <dmillerm@oberlin.edu>
2014-11-17 23:59:47 -0500

    pistons mostly finished. prefab needs to be modified to end nullreference exceptions preventing testing the pistons

--- Assets/Scripts/Block Scripts/Piston.cs
+++ Assets/Scripts/Block Scripts/Piston.cs
@@ -12,27 +12,17 @@ public class Piston : AbstractBlock {
 	private float xMult;
 	private float yMult;
 
-	void Awake(){
-		start = new Int2(this.transform.position.x, this.transform.position.y);
+	private float clock;
 
-		switch (dir) {
-			case Direction.Right:
-				xMult = 1f;
-				yMult = 0f;
-				break;
-			case Direction.Left:
-				xMult = -1f;
-				yMult = 0f;
-				break;
-			case Direction.Up:
-				xMult = 0f;
-				yMult = 1f;
-				break;
-			case Direction.Down:
-				xMult = 0f;
-				yMult = -1f;
-				break;
-		}
+	void Awake(){
+
+		blockManager = GameObject.FindObjectOfType<BlockManager>();
+
+		start = GetCurrentPosition();
+
+		setDir(dir);
+
+		clock = 1f;
 	}
 
 
@@ -49,15 +39,105 @@ public class Piston : AbstractBlock {
 
 	public override string myType ()
 	{
-		return "Block";
+		return "Piston";
 	}
 
 	void Update(){
 
 		if(!gameManager.gameFrozen) {
+
+			clock += Time.deltaTime;
+
 			//get the grid to edit as piston moves:
 			Dictionary<Int2, AbstractBlock> grid = blockManager.grid;
 
+			//time to check if we have to turn around:
+			if(clock >= 1f){
+
+				//remove last block:
+				Int2 lastBlock = new Int2(this.transform.position.x + -1*xMult,this.transform.position.y + -1*yMult);
+				grid.Remove(lastBlock);
+
+				//if arrived at start position:
+				if(GetCurrentPosition().Equals(start)){
+					switchDir();
+					//snap to grid:
+					blockSprite.position = new Vector3(start.x, start.y, 0);
+					this.transform.position = new Vector3(start.x, start.y, 0);
+
+					return;
+				}
+
+				//if something there:
+				var nextBlock = new Int2((this.transform.position.x + xMult), (this.transform.position.y + yMult));
+
+				if(grid.ContainsKey(nextBlock)){
+
+					//snap to perfect grid for this frame:
+					blockSprite.position = new Vector3(this.transform.position.x, this.transform.position.x, 0);
+
+					//switch direction:
+					switchDir();
+					return;
+					   
+				}
+
+				//add next block:
+				grid.Add(nextBlock, this);
+
+				//snap gameobject transform to current block:
+				this.transform.position = new Vector3(this.transform.position.x + 1*xMult, this.transform.position.y + 1*yMult, 0);
+
+				return;
+
+			}
+
+			blockSprite.transform.position += new Vector3(xMult*clock, yMult*clock, 0);
+
+
+
+		}
+	}
+
+	void setDir(Direction dir){
+
+		switch (dir) {
+		case Direction.Right:
+			xMult = 1f;
+			yMult = 0f;
+			break;
+		case Direction.Left:
+			xMult = -1f;
+			yMult = 0f;
+			break;
+		case Direction.Up:
+			xMult = 0f;
+			yMult = 1f;
+			break;
+		case Direction.Down:
+			xMult = 0f;
+			yMult = -1f;
+			break;
+		}
+		this.dir = dir;
+	
+	}
+
+	void switchDir(){
+
+		switch (this.dir) {
+		case Direction.Right:
+			setDir(Direction.Left);
+			break;
+		case Direction.Left:
+			setDir(Direction.Right);
+			break;
+		case Direction.Up:
+			setDir(Direction.Down);
+			break;
+		case Direction.Down:
+			setDir(Direction.Up);
+			break;
 		}
 	}
 }
